59fe9d03392dff2428d8620e32d506a9b2a0ee82
hustcc <i@hust.cc>
2025-04-28 16:10:57 +0800

    feat: add radar

--- src/index.ts
+++ src/index.ts
@@ -10,242 +10,283 @@ import {
 } from "@modelcontextprotocol/sdk/types.js";
 
 const BaseConfig = {
-  title: { type: "string", description: "Set the chart title." },
-  axisXTitle: { type: "string", description: "Set the x-axis title of chart." },
-  axisYTitle: { type: "string", description: "Set the y-axis title of chart." },
+  width: {
+    type: 'number',
+    description: 'Set the width of chart, default is 600.',
+  },
+  height: {
+    type: 'number',
+    description: 'Set the height of chart, default is 400.',
+  },
+  title: { type: 'string', description: 'Set the title of chart.' },
+  axisXTitle: { type: 'string', description: 'Set the x-axis title of chart.' },
+  axisYTitle: { type: 'string', description: 'Set the y-axis title of chart.' },
 };
 
 const Tools = [
   {
-    name: "generate_line_chart",
+    name: 'generate_line_chart',
     description:
       "Generate a line chart to show trends over time, such as the ratio of Apple computer sales to Apple's profits changed from 2000 to 2016.",
     inputSchema: {
-      type: "object",
+      type: 'object',
       properties: {
         data: {
-          type: "array",
+          type: 'array',
           items: {
-            type: "object",
+            type: 'object',
             properties: {
-              time: { type: "string" },
-              value: { type: "number" },
+              time: { type: 'string' },
+              value: { type: 'number' },
             },
           },
           description:
-            "Data for line chart, such as, [{time: '2015', value: '23'}].",
-        },
-        ...BaseConfig,
-      },
-      required: ["data"],
-    },
-  },
-  {
-    name: "generate_column_chart",
-    description:
-      "Generate a column chart, which are best for comparing categorical data, such as when values are close, column charts are preferable because our eyes are better at judging height than other visual elements like area or angles.",
-    inputSchema: {
-      type: "object",
-      properties: {
-        data: {
-          type: "array",
-          items: {
-            type: "object",
-            properties: {
-              category: { type: "string" },
-              value: { type: "number" },
-              group: { type: "string" },
-            },
-            required: ["category", "value"],
-          },
-          description:
-            "Data for column chart, such as, [{category: '北京' value: 825; group: '油车'}].",
-        },
-        group: {
-          type: "boolean",
-          description:
-            "Whether grouping is enabled. column charts require a 'group' field in the data.",
+            "Data for line chart, such as, [{ time: '2015', value: 23 }].",
         },
         stack: {
-          type: "boolean",
+          type: 'boolean',
           description:
-            "Whether stacking is enabled. column charts require a 'stack' field in the data.",
+            "Whether stacking is enabled. When enabled, line charts require a 'group' field in the data.",
         },
         ...BaseConfig,
       },
-      required: ["data"],
+      required: ['data'],
     },
   },
   {
-    name: "generate_pie_chart",
+    name: 'generate_column_chart',
     description:
-      "Generate a pie chart to show the proportion of parts, such as market share and budget allocation.",
+      'Generate a column chart, which are best for comparing categorical data, such as when values are close, column charts are preferable because our eyes are better at judging height than other visual elements like area or angles.',
     inputSchema: {
-      type: "object",
+      type: 'object',
       properties: {
         data: {
-          type: "array",
+          type: 'array',
           items: {
-            type: "object",
+            type: 'object',
             properties: {
-              category: { type: "string" },
-              value: { type: "number" },
+              category: { type: 'string' },
+              value: { type: 'number' },
+              group: { type: 'string' },
             },
-            required: ["category", "value"],
+            required: ['category', 'value'],
           },
           description:
-            "Data for pie chart, (such as, [{category: '分类一', value: 27 }])",
+            "Data for column chart, such as, [{ category: '北京' value: 825; group: '油车' }].",
+        },
+        group: {
+          type: 'boolean',
+          description:
+            "Whether grouping is enabled. When enabled, column charts require a 'group' field in the data.",
+        },
+        stack: {
+          type: 'boolean',
+          description:
+            "Whether stacking is enabled. When enabled, column charts require a 'group' field in the data.",
+        },
+        ...BaseConfig,
+      },
+      required: ['data'],
+    },
+  },
+  {
+    name: 'generate_pie_chart',
+    description:
+      'Generate a pie chart to show the proportion of parts, such as market share and budget allocation.',
+    inputSchema: {
+      type: 'object',
+      properties: {
+        data: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              category: { type: 'string' },
+              value: { type: 'number' },
+            },
+            required: ['category', 'value'],
+          },
+          description:
+            "Data for pie chart, (such as, [{ category: '分类一', value: 27 }])",
         },
         innerRadius: {
-          type: "number",
+          type: 'number',
           description:
-            "Set the pie chart as a donut chart. Set the value to 0.6 to enable it.",
+            'Set the pie chart as a donut chart. Set the value to 0.6 to enable it.',
           default: 0,
         },
         ...BaseConfig,
       },
-      required: ["data"],
+      required: ['data'],
     },
   },
   {
-    name: "generate_area_chart",
+    name: 'generate_area_chart',
     description:
       "Generate a area chart to show data trends under continuous independent variables and observe the overall data trend, such as displacement = velocity (average or instantaneous) × time: s = v × t. If the x-axis is time (t) and the y-axis is velocity (v) at each moment, an area chart allows you to observe the trend of velocity over time and infer the distance traveled by the area's size.",
     inputSchema: {
-      type: "object",
+      type: 'object',
       properties: {
         data: {
-          type: "array",
+          type: 'array',
           items: {
-            type: "object",
+            type: 'object',
             properties: {
-              time: { type: "string" },
-              value: { type: "number" },
-              group: { type: "string" },
+              time: { type: 'string' },
+              value: { type: 'number' },
+              group: { type: 'string' },
             },
-            required: ["time", "value"],
+            required: ['time', 'value'],
           },
           description:
-            "Data for pie chart, such as, [{time: '2018'; value: 99.9 }].",
+            "Data for pie chart, such as, [{ time: '2018'; value: 99.9 }].",
         },
         stack: {
-          type: "boolean",
+          type: 'boolean',
           description:
-            "Whether stacking is enabled. column charts require a 'stack' field in the data.",
+            "Whether stacking is enabled. When enabled, area charts require a 'group' field in the data.",
         },
         ...BaseConfig,
       },
-      required: ["data"],
+      required: ['data'],
     },
   },
   {
-    name: "generate_bar_chart",
+    name: 'generate_bar_chart',
     description:
-      "Generate a bar chart to show data for numerical comparisons among different categories, such as comparing categorical data and for horizontal comparisons.",
+      'Generate a bar chart to show data for numerical comparisons among different categories, such as comparing categorical data and for horizontal comparisons.',
     inputSchema: {
-      type: "object",
+      type: 'object',
       properties: {
         data: {
-          type: "array",
+          type: 'array',
           items: {
-            type: "object",
+            type: 'object',
             properties: {
-              category: { type: "string" },
-              value: { type: "number" },
-              group: { type: "string" },
+              category: { type: 'string' },
+              value: { type: 'number' },
+              group: { type: 'string' },
             },
-            required: ["category", "value"],
+            required: ['category', 'value'],
           },
           description:
-            "Data for bar chart, such as, [{category: '分类一'; value: 10 }].",
+            "Data for bar chart, such as, [{ category: '分类一'; value: 10 }].",
         },
         group: {
-          type: "boolean",
+          type: 'boolean',
           description:
-            "Whether grouping is enabled. column charts require a 'group' field in the data.",
+            "Whether grouping is enabled. When enabled, bar charts require a 'group' field in the data.",
         },
         stack: {
-          type: "boolean",
+          type: 'boolean',
           description:
-            "Whether stacking is enabled. column charts require a 'stack' field in the data.",
+            "Whether stacking is enabled. When enabled, bar charts require a 'group' field in the data.",
         },
         ...BaseConfig,
       },
-      required: ["data"],
+      required: ['data'],
     },
   },
   {
-    name: "generate_histogram_chart",
+    name: 'generate_histogram_chart',
     description:
-      "Generate a histogram chart to show the frequency of data points within a certain range. It can observe data distribution, such as normal and skewed distributions, and identify data concentration areas and extreme points.",
+      'Generate a histogram chart to show the frequency of data points within a certain range. It can observe data distribution, such as normal and skewed distributions, and identify data concentration areas and extreme points.',
     inputSchema: {
-      type: "object",
+      type: 'object',
       properties: {
         data: {
-          type: "array",
-          description: "Data for bar chart, such as, [78, 88, 60, 100, 95].",
+          type: 'array',
+          description: 'Data for bar chart, such as, [ 78, 88, 60, 100, 95 ].',
         },
         binNumber: {
-          type: "number",
-          description: "Number of intervals to define the number of intervals in a histogram.",
+          type: 'number',
+          description:
+            'Number of intervals to define the number of intervals in a histogram.',
         },
         ...BaseConfig,
       },
-      required: ["data"],
+      required: ['data'],
     },
   },
   {
-    name: "generate_scatter_chart",
+    name: 'generate_scatter_chart',
     description:
-      "Generate a scatter chart to show the relationship between two variables, helps discover their relationship or trends, such as the strength of correlation, data distribution patterns, etc.",
+      'Generate a scatter chart to show the relationship between two variables, helps discover their relationship or trends, such as the strength of correlation, data distribution patterns, etc.',
     inputSchema: {
-      type: "object",
+      type: 'object',
       properties: {
         data: {
-          type: "array",
+          type: 'array',
           items: {
-            type: "object",
+            type: 'object',
             item: {
               properties: {
-                x: { type: "number" },
-                y: { type: "number" },
+                x: { type: 'number' },
+                y: { type: 'number' },
               },
-              required: ["x", "y"],
-            }
+              required: ['x', 'y'],
+            },
           },
-          description: "Data for scatter chart, such as, [{ x: 10, y: 15 }].",
+          description: 'Data for scatter chart, such as, [{ x: 10, y: 15 }].',
         },
         ...BaseConfig,
       },
-      required: ["data"],
+      required: ['data'],
     },
   },
   {
-    name: "generate_word_cloud_chart",
+    name: 'generate_word_cloud_chart',
     description:
-      "Generate a word cloud chart to show word frequency or weight through text size variation, such as, analyzing common words in social media, reviews, or feedback.",
+      'Generate a word cloud chart to show word frequency or weight through text size variation, such as, analyzing common words in social media, reviews, or feedback.',
     inputSchema: {
-      type: "object",
+      type: 'object',
       properties: {
         data: {
-          type: "array",
+          type: 'array',
           items: {
-            type: "object",
+            type: 'object',
             item: {
               properties: {
-                value: { type: "string" },
-                text: { type: "string" },
+                value: { type: 'string' },
+                text: { type: 'string' },
               },
-              required: ["value", "text"],
-            }
+              required: ['value', 'text'],
+            },
           },
-          description: "Data for word cloud chart, such as, [{ value: '4.272', text: '形成' }].",
+          description:
+            "Data for word cloud chart, such as, [{ value: '4.272', text: '形成' }].",
         },
         ...BaseConfig,
       },
-      required: ["data"],
+      required: ['data'],
     },
-  }
+  },
+  {
+    name: 'generate_radar_chart',
+    description:
+      'Generate a radar chart to display multidimensional data (four dimensions or more), such as, evaluate Huawei and Apple phones in terms of five dimensions: ease of use, functionality, camera, benchmark scores, and battery life.',
+    inputSchema: {
+      type: 'object',
+      properties: {
+        data: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              name: { type: 'string' },
+              value: { type: 'number' },
+              group: { type: 'string' },
+            },
+            required: ['name', 'value'],
+          },
+          description:
+            "Data for radar chart, such as, [{ name: 'Design', value: 70 }].",
+        },
+        ...BaseConfig,
+      },
+      required: ['data'],
+    },
+  },
 ];
 
 /**
@@ -299,15 +340,15 @@ class McpServerChart {
 
     this.server.setRequestHandler(CallToolRequestSchema, async (request) => {
       const ChartTypeMapping = {
-        generate_line_chart: "line",
-        generate_column_chart: "column",
-        generate_area_chart: "area",
-        generate_pie_chart: "pie",
-        generate_bar_chart: "bar",
-        generate_histogram_chart: "histogram",
-        generate_scatter_chart: "scatter",
-        generate_word_cloud_chart: "word-cloud"
-
+        generate_line_chart: 'line',
+        generate_column_chart: 'column',
+        generate_area_chart: 'area',
+        generate_pie_chart: 'pie',
+        generate_bar_chart: 'bar',
+        generate_histogram_chart: 'histogram',
+        generate_scatter_chart: 'scatter',
+        generate_word_cloud_chart: 'word-cloud',
+        generate_radar_chart: 'radar',
       } as any;
 
       const type = ChartTypeMapping[request.params.name];

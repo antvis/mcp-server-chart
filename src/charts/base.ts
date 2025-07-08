fbb7ea7d2d64e650327d0360066df6ae49321b10
bqx <boqingxin14@gmail.com>
2025-07-08 16:38:54 +0800

    feat: add texture option to chart schema

--- src/charts/base.ts
+++ src/charts/base.ts
@@ -7,6 +7,12 @@ export const ThemeSchema = z
   .default("default")
   .describe("Set the theme for the chart, optional, default is 'default'.");
 
+export const TextureSchema = z
+  .enum(["default", "rough"])
+  .optional()
+  .default("default")
+  .describe("Set the texture for the chart, optional, default is 'default'.");
+
 export const WidthSchema = z
   .number()
   .optional()

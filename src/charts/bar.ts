ad3385301fb2ec90c4c9f92229c13a2184d56327
hustcc <i@hust.cc>
2025-07-25 15:30:39 +0800

    chore: update

--- src/charts/bar.ts
+++ src/charts/bar.ts
@@ -61,7 +61,7 @@ const schema = {
 const tool = {
   name: "generate_bar_chart",
   description:
-    "Generate a bar chart to show data for numerical comparisons among different categories, such as, comparing categorical data and for horizontal comparisons.",
+    "Generate a horizontal bar chart to show data for numerical comparisons among different categories, such as, comparing categorical data and for horizontal comparisons.",
   inputSchema: zodToJsonSchema(schema),
 };
 

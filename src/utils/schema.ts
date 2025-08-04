717a5658d991dce1725279595bbec7b8b14ea210
Rain120 <1085131904@qq.com>
2025-08-04 16:25:11 +0800

    feat: migrate zod to v4

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,10 +1,11 @@
-import { z } from "zod";
-import { zodToJsonSchema as zodToJsonSchemaOriginal } from "zod-to-json-schema";
+import * as z from "zod";
 
-// TODO: use zod v4 JSON to schema to replace zod-to-json-schema when v4 is stable
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export const zodToJsonSchema = (schema: Record<string, z.ZodType<any>>) => {
-  return zodToJsonSchemaOriginal(z.object(schema), {
-    rejectedAdditionalProperties: undefined,
+  return z.toJSONSchema(z.object(schema), {
+    override: (ctx) => {
+      if (ctx?.jsonSchema?.type === 'object') {
+        ctx.jsonSchema.additionalProperties = undefined;
+      }
+    }
   });
 };

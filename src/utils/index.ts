a5af6c534a868740b3f70feea3016769e9affdeb
Rain120 <1085131904@qq.com>
2025-08-14 21:29:51 +0800

    feat: 移除zodToJsonSchema方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 export { callTool } from "./callTool";
 export { generateChartUrl } from "./generate";
-export { zodToJsonSchema } from "./schema";
 export { InMemoryEventStore } from "./InMemoryEventStore";
 export { getBody } from "./getBody";
 export { createBaseHttpServer, type RequestHandlers } from "./httpServer";
